d5bc3ffc47180bf8184ce92b65d7d75884fa189e
DESKTOP-OGTMA5V\Ran <yellowzerow@gmail.com>
2022-11-02 17:33:52 +0900

    init 설정과 Scraping 간단 실습

---
+++ src/main/java/com/zerobase/dividend/DividendApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.dividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendApplication.class, args);
+    }
+
+}
